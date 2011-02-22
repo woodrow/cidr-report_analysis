9f43c360f4e7efcfab710333abb785a05e18c107
Stephen Woodrow <srwoodrow@gmail.com>
2011-02-22 05:20:44 -0500

    Fixed major rookie mistake of forgetting to enclose the worker processes in a while True loop.

--- cidr_analysis/utils/divide_and_conquer.py
+++ cidr_analysis/utils/divide_and_conquer.py
@@ -11,42 +11,47 @@ import time
 NUM_PROCESSES = 10
 
 def chunk_split_worker(in_queue):
-    try:
-        path = in_queue.get_nowait()
-        subprocess.check_call('split -l 25000000 {0} {0}-chunk_'.format(path),
-            shell=True)
-    except Queue.Empty:
-        return
+    while True:
+        try:
+            path = in_queue.get_nowait()
+            subprocess.check_call(
+                'split -l 25000000 {0} {0}-chunk_'.format(path), shell=True)
+        except Queue.Empty:
+            return
 
 
 def chunk_sort_worker(in_queue, out_queue):
-    try:
-        path = in_queue.get_nowait()
-        out_path = path + '-sorted'
-        subprocess.check_call("sort -T /home/cidr_analysis/tmp/ " +
-            "-t ',' -s -k 1,1 -o {1} {0}".format(path, out_path), shell=True)
-#        subprocess.check_call('rm {0}'.format(path), shell=True)
-        out_queue.put_nowait(out_path)
-    except Queue.Empty:
-        return
+    while True:
+        try:
+            path = in_queue.get_nowait()
+            out_path = path + '-sorted'
+            subprocess.check_call("sort -T /home/cidr_analysis/tmp/ " +
+                "-t ',' -s -k 1,1 -o {1} {0}".format(path, out_path),
+                shell=True)
+    #        subprocess.check_call('rm {0}'.format(path), shell=True)
+            out_queue.put_nowait(out_path)
+        except Queue.Empty:
+            return
 
 
 def chunk_merge_worker(in_queue, out_queue):
-    try:
-        path_1 = path_2 = None
-        path_1 = in_queue.get_nowait()
-        path_2 = in_queue.get_nowait()
-        out_hash = hashlib.sha1(path_1 + path_2).hexdigest()
-        out_path = os.path.join(os.path.dirname(path_1), out_hash)
-        subprocess.check_call("sort -m -T /home/cidr_analysis/tmp/ " +
-            "-t ',' -s -k 1,1 -o {2} {0} {1}".format(path_1, path_2, out_path),
-            shell=True)
-        subprocess.check_call('rm {0} {1}'.format(path_1, path_2), shell=True)
-        out_queue.put_nowait(out_path)
-    except Queue.Empty:
-        if path_1:
-            out_queue.put_nowait(path_1)
-        return
+    while True:
+        try:
+            path_1 = path_2 = None
+            path_1 = in_queue.get_nowait()
+            path_2 = in_queue.get_nowait()
+            out_hash = hashlib.sha1(path_1 + path_2).hexdigest()
+            out_path = os.path.join(os.path.dirname(path_1), out_hash)
+            subprocess.check_call("sort -m -T /home/cidr_analysis/tmp/ " +
+                "-t ',' -s -k 1,1 -o {2} {0} {1}".format(
+                path_1, path_2, out_path), shell=True)
+            subprocess.check_call('rm {0} {1}'.format(path_1, path_2),
+                shell=True)
+            out_queue.put_nowait(out_path)
+        except Queue.Empty:
+            if path_1:
+                out_queue.put_nowait(path_1)
+            return
 
 
 def print_deltat(msg, t1, t2):
@@ -64,6 +69,7 @@ def main():
     for path in input_paths:
         subprocess.check_call('split -l 25000000 {0} {0}-chunk_'.format(path),
             shell=True)
+#######################################
 #    processes = []
 #    for i in xrange(NUM_PROCESSES):
 #        p = multiprocessing.Process(target=chunk_split_worker,
@@ -72,6 +78,7 @@ def main():
 #        processes.append(p)
 #    for p in processes:
 #        p.join()
+#######################################
     t2 = time.time()
     print_deltat("Splitting input files", t1, t2)
 
