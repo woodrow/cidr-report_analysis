703f273e9e21c8bad2b145b31cc07f8559068f13
Stephen Woodrow <srwoodrow@gmail.com>
2011-02-28 03:04:28 -0500

    Rename weeks_seen to weeks_visible. Add last_seen and total_weeks_visible fields to output.

--- cidr_analysis/utils/prefix_history.py
+++ cidr_analysis/utils/prefix_history.py
@@ -12,12 +12,13 @@ POweek = collections.namedtuple('POweek', 'peerid origin_as ord_date')
 
 class PORecord(object):
     def __init__(self, prefix, peerid, origin_as, date_effective,
-        weeks_seen=''):
+        weeks_visible=''):
         self.prefix = prefix
         self.peerid = peerid
         self.origin_as = origin_as
         self.date_effective = date_effective
-        self.weeks_seen = weeks_seen
+        self.last_seen = date_effective
+        self.weeks_visible = weeks_visible
 
 
 #def process_prefix(prefix, prefix_lines, end_date, path):
@@ -25,35 +26,37 @@ def process_prefix(prefix, prefix_lines, end_date, row_serial):
     prefix_lines.sort(key=lambda x: x.peerid * 1e6 + x.ord_date)
     current_porecord = PORecord(
         prefix, prefix_lines[0].peerid, prefix_lines[0].origin_as,
-        datetime.date.fromordinal(prefix_lines[0].ord_date), weeks_seen='1')
+        datetime.date.fromordinal(prefix_lines[0].ord_date), weeks_visible='1')
     complete_porecord = None
 
     for p in prefix_lines[1:]:
         if p.peerid != current_porecord.peerid:
-            # fill current POrecord.weeks_seen with 0s until end_date
-            current_porecord.weeks_seen += '0' * (
+            # fill current POrecord.weeks_visible with 0s until end_date
+            current_porecord.weeks_visible += '0' * (
                 ((end_date - current_porecord.date_effective).days // 7 + 1) -
-                len(current_porecord.weeks_seen))
+                len(current_porecord.weeks_visible))
             complete_porecord = current_porecord
         elif p.origin_as != current_porecord.origin_as:
-            # fill current POrecord.weeks_seen with 0s until p.ord_date
-            current_porecord.weeks_seen += '0' * (
+            # fill current POrecord.weeks_visible with 0s until p.ord_date
+            current_porecord.weeks_visible += '0' * (
                 ((datetime.date.fromordinal(p.ord_date) -
                 current_porecord.date_effective).days // 7 + 1) -
-                len(current_porecord.weeks_seen))
+                len(current_porecord.weeks_visible))
             complete_porecord = current_porecord
 
         if complete_porecord:
-            print(
-                ("{1[0]},{0.prefix},{0.peerid},{0.origin_as},{0.date_effective},"
-                "{0.weeks_seen}").format(complete_porecord, row_serial))
+            print((
+                "{1[0]},{0.prefix},{0.peerid},{0.origin_as},"
+                "{0.date_effective},{0.last_seen},{0.weeks_visible},{2}"
+                ).format(complete_porecord, row_serial,
+                sum((1 for x in complete_porecord.weeks_visible if x == '1'))))
             row_serial[0] += 1
-#            weeks_seen = sum((1 for x in complete_porecord.weeks_seen if x == '1'))
+#            weeks_visible = sum((1 for x in complete_porecord.weeks_visible if x == '1'))
 #            weeks_grepped = int(subprocess.Popen(
 #                "grep '{0.prefix},{0.peerid},{0.origin_as}' {1} | wc -l".format(
 #                complete_porecord, path), shell=True,
 #                stdout=subprocess.PIPE).communicate()[0].strip())
-#            if weeks_seen != weeks_grepped:
+#            if weeks_visible != weeks_grepped:
 #                print("ERROR in above prefix!!!")
             current_porecord = PORecord(
                 prefix, p.peerid, p.origin_as,
@@ -62,25 +65,28 @@ def process_prefix(prefix, prefix_lines, end_date, row_serial):
 
         weeks_since_last_record = (
             (p.ord_date - current_porecord.date_effective.toordinal()) // 7 -
-            len(current_porecord.weeks_seen))
+            len(current_porecord.weeks_visible))
         if weeks_since_last_record + 1 >= 0:
-            current_porecord.weeks_seen += '0' * weeks_since_last_record
-            current_porecord.weeks_seen += '1'
+            current_porecord.weeks_visible += '0' * weeks_since_last_record
+            current_porecord.weeks_visible += '1'
+        current_porecord.last_seen = datetime.date.fromordinal(p.ord_date)
 
     # handle last partially incomplete object
-    current_porecord.weeks_seen += '0' * (
+    current_porecord.weeks_visible += '0' * (
         ((end_date - current_porecord.date_effective).days // 7 + 1) -
-        len(current_porecord.weeks_seen))
-    print(
-        ("{1[0]},{0.prefix},{0.peerid},{0.origin_as},{0.date_effective},"
-        "{0.weeks_seen}").format(current_porecord, row_serial))
+        len(current_porecord.weeks_visible))
+    print((
+        "{1[0]},{0.prefix},{0.peerid},{0.origin_as},{0.date_effective},"
+        "{0.last_seen},{0.weeks_visible},{2}"
+        ).format(current_porecord, row_serial,
+        sum((1 for x in current_porecord.weeks_visible if x == '1'))))
     row_serial[0] += 1
-#    weeks_seen = sum((1 for x in current_porecord.weeks_seen if x == '1'))
+#    weeks_visible = sum((1 for x in current_porecord.weeks_visible if x == '1'))
 #    weeks_grepped = int(subprocess.Popen(
 #        "grep '{0.prefix},{0.peerid},{0.origin_as}' {1} | wc -l".format(
 #        current_porecord, path), shell=True,
 #        stdout=subprocess.PIPE).communicate()[0].strip())
-#    if weeks_seen != weeks_grepped:
+#    if weeks_visible != weeks_grepped:
 #        print("ERROR in above prefix!!!")
 
 
@@ -96,7 +102,8 @@ def gen_prefix_origin_records(path, end_date):
                 if current_prefix_lines:
                     # record = process_prefix
                     process_prefix(
-                        current_prefix, current_prefix_lines, end_date, row_serial)
+                        current_prefix, current_prefix_lines, end_date,
+                        row_serial)
                 current_prefix = components[0]
                 current_prefix_lines = []
 
