9dfb413dd0efaeaa5c4cf8486b720aa1dab87ded
Stephen Woodrow <srwoodrow@gmail.com>
2011-04-04 14:40:48 -0400

    Improve validity comparison R and SQL files.

--- analysis/gen_cidr_report_validity/cidr_report_compare.sql
+++ analysis/gen_cidr_report_validity/cidr_report_compare.sql
@@ -6,12 +6,28 @@ FROM (
     SELECT
         gcr.date,
         (gcr.date - '1997-11-14')/7 as week,
+
         sum(abs(gcr.nets_current - ecr.netsnow)) as delta_netsnow_top30,
+        sum(case when (gcr.nets_current - ecr.netsnow) > 0 then
+            (gcr.nets_current - ecr.netsnow) else 0 end)
+            as gcr_greater_netsnow,
+        sum(case when (ecr.netsnow - gcr.nets_current) > 0 then
+            (ecr.netsnow - gcr.nets_current) else 0 end)
+            as ecr_greater_netsnow,
+
         sum(abs(gcr.nets_reduced - ecr.netgain)) as delta_netgain_top30,
+        sum(case when (gcr.nets_reduced - ecr.netgain) > 0 then
+            (gcr.nets_reduced - ecr.netgain) else 0 end)
+            as gcr_greater_netgain,
+        sum(case when (ecr.netgain - gcr.nets_reduced) > 0 then
+            (ecr.netgain - gcr.nets_reduced) else 0 end)
+            as ecr_greater_netgain,
+
         sum(case when gcr.rank_netgain >= 30 then 1 else 0 end)
             as not_in_top30,
         sum(case when gcr.rank_netgain >= 30 then ecr.rank else 0 end)
             as missing_ranks,
+
         sum(ecr.netsnow) as prefixes_ecr_top30,
         sum(gcr.nets_current) as prefixes_gcr_top30
     FROM
@@ -20,6 +36,7 @@ FROM (
     WHERE
         gcr.date = ecr.date
         AND gcr.origin_as = ecr.origin_as
+        AND gcr.id >= 12415379
     GROUP BY gcr.date
     ORDER BY gcr.date
 ) AS cmp, (
@@ -28,6 +45,8 @@ FROM (
         sum(gcr.nets_current) as prefixes_gcr_total
     FROM
         gen_cidr_reports as gcr
+    WHERE
+        gcr.id >= 12415379
     GROUP BY gcr.date
 ) AS tp
 WHERE
--- analysis/gen_cidr_report_validity/cidr_report_validity.r
+++ analysis/gen_cidr_report_validity/cidr_report_validity.r
@@ -1,43 +1,173 @@
 if(!exists('cidr.compare')) {
-    cidr.compare <- read.csv("cidr_report_compare.csv",
-        colClasses=c("Date", "integer", "integer", "integer", "integer",
-        "integer", "integer", "integer", "integer", "numeric"))
+    cidr.compare = read.csv("cidr_report_compare_newagg.csv",
+        colClasses=c(
+        "Date",     # date
+        "integer",  # week
+        "integer",  # delta_netsnow_top30
+        "integer",  # gcr_greater_netsnow
+        "integer",  # ecr_greater_netsnow
+        "integer",  # delta_netgain_top30
+        "integer",  # gcr_greater_netgain
+        "integer",  # ecr_greater_netgain
+        "integer",  # not_in_top30
+        "integer",  # missing_ranks
+        "integer",  # prefixes_ecr_top30
+        "integer",  # prefixes_gcr_top30
+        "integer",  # prefixes_gcr_total
+        "numeric"   # avg_missing
+        ))
 }
 
 plot_cr_error <- function() {
-    ylims=c(
-        min(cidr.compare$delta_netsnow_top30, cidr.compare$delta_netgain_top30),
-        max(cidr.compare$delta_netsnow_top30, cidr.compare$delta_netgain_top30))
-    par(mar=c(5,4,4,4)+0.1)
-    plot(cidr.compare$date,
-        rep(1,length(cidr.compare$date)), type="h", col="grey95",
-        ylim=c(0,1), xaxt="n", yaxt="n", xlab="", ylab="")
+#### SETUP PLOT
+    #par(mar=c(5,4,4,4)+0.1)
+    layout(
+        matrix(c(1,2), 2, 1, byrow = TRUE)
+        widths=c(1),
+        heights=c(2,4))
+    ylims = c(
+        min(-1*cidr.compare$ecr_greater_netsnow,
+            -1*cidr.compare$ecr_greater_netgain),
+        max(cidr.compare$gcr_greater_netsnow,
+            cidr.compare$gcr_greater_netgain))
+    unit_ylims = c(ylims[1] / ylims[2], 1)
+
+#### PLOT 1
+    par(mar=c(0,5,2,3))
+    # PLOT data available; light grey
+    plot(
+        cidr.compare$date,
+        rep(1,length(cidr.compare$date)),
+        type="h",
+        col="grey95",
+        ylim=c(0,1),
+        xaxt="n",
+        yaxt="n",
+        xlab="",
+        ylab="")
+    # PLOT #ASes not in top 30; dark grey
     par(new=TRUE)
-    plot(cidr.compare$date,
-        cidr.compare$not_in_top30, type="h", col="grey70", ylim=c(0,30),
-        xaxt="n", yaxt="n", xlab="", ylab="")
-    axis(4, pretty(c(0,30),10))
-    mtext("ASes not in top 30", side=4, line=3, font=1)
+    plot(
+        cidr.compare$date,
+        cidr.compare$not_in_top30,
+        type="h",
+        col="grey70",
+        ylim=c(0,30),
+        xaxt="n",
+        xlab="",
+        ylab="#ASes",
+        main="Number of ASes on the Huston/Bates CIDR Report not in the top 30 of the Woodrow CIDR Report")
+    mtext("", side=4, line=3, font=1)
+
+#### PLOT 2
+    par(mar=c(3,5,2,3))
+    # PLOT data available; light grey
+    plot(
+        cidr.compare$date,
+        rep(1,length(cidr.compare$date)),
+        type="h",
+        col="grey95",
+        ylim=c(0,1),
+        xaxt="n",
+        yaxt="n",
+        xlab="",
+        ylab="")
+    # PLOT delta netsnow prefixes for ASes where GCR > ECR; blue
     par(new=TRUE)
-    plot(cidr.compare$date, cidr.compare$delta_netsnow_top30, type='l',
-        col='blue', ylim=ylims, xlab="",
+    plot(
+        cidr.compare$date,
+        cidr.compare$gcr_greater_netsnow,
+        type='l',
+        col='blue',
+        ylim=ylims,
+        xlab="",
         ylab=expression(paste(Sigma,"|", Delta,"prefixes|")),
-        main="Comparison of Generated and Emailed CIDR Reports")
+        main="Comparison of Woodrow and Huston/Bates CIDR Reports")
+    # PLOT delta netsnow prefixes for ASes where GCR < ECR; blue
     par(new=TRUE)
-    plot(cidr.compare$date, cidr.compare$delta_netgain_top30, type='l',
-        col='red', ylim=ylims, xaxt="n", yaxt="n", xlab="", ylab="")
+    plot(
+        cidr.compare$date,
+        -1*cidr.compare$ecr_greater_netsnow,
+        type='l',
+        col='blue',
+        ylim=ylims,
+        xlab="",
+        ylab="",
+        xaxt="n",
+        yaxt="n")
+    # PLOT delta netgain prefixes for ASes where GCR > ECR; red
     par(new=TRUE)
-    plot(cidr.compare$date,
-        cidr.compare$delta_netgain_top30/cidr.compare$prefixes_gcr_top30,
-        type='l', col='red', ylim=c(0,1), xaxt="n", yaxt="n", lty=2,
-        xlab="", ylab="")
+    plot(
+        cidr.compare$date,
+        cidr.compare$gcr_greater_netgain,
+        type='l',
+        col='red',
+        ylim=ylims,
+        xaxt="n",
+        yaxt="n",
+        xlab="",
+        ylab="")
+    # PLOT delta netgain prefixes for ASes where GCR < ECR; red
+    par(new=TRUE)
+    plot(
+        cidr.compare$date,
+        -1*cidr.compare$ecr_greater_netgain,
+        type='l',
+        col='red',
+        ylim=ylims,
+        xaxt="n",
+        yaxt="n",
+        xlab="",
+        ylab="")
+
+
+    # PLOT fraction of total netsnow contributed by top30; green
+    par(new=TRUE)
+    plot(
+        cidr.compare$date,
+        cidr.compare$prefixes_gcr_top30/cidr.compare$prefixes_gcr_total,
+        type='l',
+        col='green',
+        ylim=unit_ylims,
+        xaxt="n",
+        yaxt="n",
+        xlab="",
+        ylab="")
+    axis(4, pretty(unit_ylims,5))
+#    # PLOT ratio/fraction of delta_netgain in the top30 to netsnow in the top30
+#    par(new=TRUE)
+#    plot(
+#        cidr.compare$date,
+#        cidr.compare$delta_netgain_top30/cidr.compare$prefixes_gcr_top30,
+#        type='l',
+#        col='red',
+#        ylim=unit_ylims,
+#        xaxt="n",
+#        yaxt="n",
+#        lty=2,
+#        xlab="",
+#        ylab="")
+#
     legend(
         'topright',
-        c(expression(Delta~netsnow[top30]),
-        expression(Delta~netgain[top30]),
-        expression(Delta~netgain/netsnow[top30])),
-        col=c("blue","red", "red"),
-        lty=c(1, 1, 2),
+        c(
+            expression(Delta~netsnow[top30]),
+            expression(Delta~netgain[top30]),
+#            expression(Delta~netgain/netsnow[top30]),
+            expression(netsnow[top30]/netsnow[total])
+            ),
+        col=c(
+            "blue",
+            "red",
+#            "red",
+            "green"
+            ),
+        lty=c(
+            1,
+            1,
+#            2,
+            1
+            ),
         lwd=1:1,
         bg="white",
         inset=0.02)
