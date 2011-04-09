b04717d70ecf6de9d3fe52f5abf53933b55e60a6
Stephen Woodrow <srwoodrow@gmail.com>
2011-04-08 20:26:19 -0400

    significant updates to processing for gen_cidr_report data.

--- analysis/correlation.r
+++ analysis/correlation.r
@@ -1,42 +1,40 @@
-data_sets = c("email_cidr_report")
-for(e in data_sets) {
-    if(!exists(e)) {
-        library(RdbiPgSQL)
-        conn <- dbConnect(
-            PgSQL(),
-            host="mitas-2.csail.mit.edu",
-            dbname="woodrow",
-            user="woodrow",
-            password="woodrow$mitas-2@2011"
-        )
-        ########################################################################
-        res = dbSendQuery(conn, paste(
-            "SELECT date, origin_as, rank",
-            "FROM email_cidr_reports",
-            "ORDER BY origin_as, date;"))
-        email_cidr_report = dbGetResult(res)
-        email_cidr_report$date = as.Date(email_cidr_report$date, '%m-%d-%Y')
-        ecr = split(email_cidr_report,
-                factor(email_cidr_report$origin_as, levels=unique(email_cidr_report$origin_as)))
-        epoch = min(email_cidr_report$date)
-        epoch = epoch - as.POSIXlt(epoch)$wday
-        epoch = epoch - 7  # bring it back one week before
-        end_epoch = as.Date('2011-01-31')
-        ########################################################################
-        dbDisconnect(conn)
-        break
-    }
+UNIX_EPOCH = as.Date('1970-01-01')
+
+get.ecr <- function() {
+    library(RdbiPgSQL)
+    conn <- dbConnect(
+        PgSQL(),
+        host="mitas-2.csail.mit.edu",
+        dbname="woodrow",
+        user="woodrow",
+        password="woodrow$mitas-2@2011"
+    )
+    ########################################################################
+    res = dbSendQuery(conn, paste(
+        "SELECT date, origin_as, rank",
+        "FROM email_cidr_reports",
+        "ORDER BY origin_as, date;"))
+    email_cidr_report <<- dbGetResult(res)
+    email_cidr_report$date = as.Date(email_cidr_report$date, '%m-%d-%Y')
+    ecr = split(email_cidr_report,
+            factor(email_cidr_report$origin_as, levels=unique(email_cidr_report$origin_as)))
+    epoch <<- min(email_cidr_report$date)
+    epoch = epoch - as.POSIXlt(epoch)$wday
+    epoch = epoch - 7  # bring it back one week before
+    end_epoch <<- as.Date('2011-01-31')
+    ########################################################################
+    dbDisconnect(conn)
+    return(ecr)
 }
 
-preprocess.ecr <- function() {
-    MAGIC_NA = 100
+preprocess.ecr <- function(ecr) {
     dates = as.Date(seq(as.numeric(epoch), as.numeric(end_epoch)),
-        origin='1970-01-01')
+        origin=UNIX_EPOCH)
     weeks = seq(1, ceiling((end_epoch-epoch)/7))
     date_weeks = as.Date((weeks-1)*7, origin=epoch)
     cecr = list()#(weeks=date_weeks)  # canonical ECR, by week
     for(origin_as in names(ecr)) {
-        cecr[[origin_as]] = rep(MAGIC_NA, length(weeks))
+        cecr[[origin_as]] = rep(NA, length(weeks))
         for(ri in c(1:nrow(ecr[[origin_as]]))) {
             row = ecr[[origin_as]][ri,]
             cecr[[origin_as]][ceiling((row$date-epoch)/7)] = row$rank
@@ -44,11 +42,11 @@ preprocess.ecr <- function() {
     }
     # fill in the blanks
     available_dates = rep(F, length(weeks))
-    for(d in unique(email_cidr_report$date)) {
-        available_dates[ceiling((as.Date(d, origin='1970-01-01')-epoch)/7)] = T
+    for(d in unique(as.Date(email_cidr_report$date, '%m-%d-%Y'))) {
+        available_dates[ceiling((as.Date(d, origin=UNIX_EPOCH)-epoch)/7)] = T
     }
     for(origin_as in names(cecr)) {
-        last_val = MAGIC_NA
+        last_val = NA
         for(ri in c(1:length(cecr[[origin_as]]))) {
             if(available_dates[ri] == F) {
                 cecr[[origin_as]][ri] = last_val
@@ -72,7 +70,7 @@ find.appearances <- function(pattern, dates) {
     duration = 0
     gap_duration = 0
     for(i in c(1:length(pattern))) {
-        if(pattern[i] < 30) {
+        if(!is.na(pattern[i])) {
             if(start == 0) {
                 start = i
             }
@@ -92,19 +90,122 @@ find.appearances <- function(pattern, dates) {
             }
         }
     }
-    appearances[['date']] = as.Date(appearances[['date']], '1970-01-01')
-    return(appearances)
+
+    if(apcount > 0) {
+        appearances[['date']] = as.Date(appearances[['date']], UNIX_EPOCH)
+        return(appearances)
+    } else {
+        return(NA)
+    }
+}
+
+
+is.nottrue <- function(x) {
+    (x == T)[!is.na(x==T)]
+}
+
+
+get.gcr <- function(origin_ases) {
+    library(RdbiPgSQL)
+    conn <- dbConnect(
+        PgSQL(),
+        host="mitas-2.csail.mit.edu",
+        dbname="woodrow",
+        user="woodrow",
+        password="woodrow$mitas-2@2011"
+    )
+    ########################################################################
+    res = dbSendQuery(conn, paste(
+        "SELECT date, origin_as, rank_netgain, rank_netsnow,",
+            "nets_current, nets_reduced",
+        "FROM gen_cidr_reports",
+        "WHERE id >= 12415379",
+        "AND origin_as in (", paste(origin_ases, collapse=","), ")",
+        "ORDER BY origin_as, date;"))
+    gen_cidr_report <<- dbGetResult(res)
+    gen_cidr_report$date = as.Date(gen_cidr_report$date, '%m-%d-%Y')
+    gcr = split(gen_cidr_report,
+        factor(gen_cidr_report$origin_as,
+        levels=unique(gen_cidr_report$origin_as)))
+    ########################################################################
+    dbDisconnect(conn)
+    return(gcr)
+}
+
+################################################################################
+################################################################################
+# NOTES TO SELF
+#
+# - USE DATA FRAMES INSTEAD OF LISTS TO ALLOW ROW INDEXING
+#
+################################################################################
+################################################################################
+
+preprocess.gcr <- function(gcr) {
+    dates = as.Date(seq(as.numeric(epoch), as.numeric(end_epoch)),
+        origin=UNIX_EPOCH)
+    weeks = seq(1, ceiling((end_epoch-epoch)/7))
+    date_weeks = as.Date((weeks-1)*7, origin=epoch)
+    cgcr = list()#(weeks=date_weeks)  # canonical ECR, by week
+    for(origin_as in names(gcr)) {
+        print(origin_as)
+        as_data = data.frame(
+            rank_netgain=as.integer(NA),
+            rank_netsnow=as.integer(NA),
+            netsnow=as.integer(NA),
+            netgain=as.integer(NA))[rep(NA,length(weeks)),]
+        for(ri in c(1:nrow(gcr[[origin_as]]))) {
+            row = gcr[[origin_as]][ri,]
+            as_data[ceiling((row$date-epoch)/7),] = row[3:6]
+        }
+        cgcr[[origin_as]] = as_data
+    }
+    # fill in the blanks
+    available_dates = rep(F, length(weeks))
+    for(d in unique(as.Date(gen_cidr_report$date, '%m-%d-%Y'))) {
+        available_dates[ceiling((as.Date(d, origin='1970-01-01')-epoch)/7)] = T
+    }
+    for(origin_as in names(cgcr)) {
+        last_row = rep(NA, 4)
+        for(ri in c(1:nrow(cgcr[[origin_as]]))) {
+            if(available_dates[ri] == F) {
+                cgcr[[origin_as]][ri,] = last_row
+            }
+            last_row = cgcr[[origin_as]][ri,]
+        }
+    }
+    cgcr[['weeks']] = date_weeks
+    return(cgcr)
 }
 
 do.stuff <- function() {
+    print("cecr")
     if(!exists('cecr')) {
-        cecr = preprocess.ecr()
+        ecr = get.ecr()
+        cecr <<- preprocess.ecr(ecr)
     }
-    appearances = list()
-    for(origin_as in names(cecr)) {
-        appearances[[origin_as]] = find.appearances(cecr[[origin_as]])
+    print("cecr done")
+    print("appearances")
+    if(!exists('appearances')) {
+        appearances = list()
+        for(origin_as in names(cecr)) {
+            appearance = find.appearances(
+                cecr[[origin_as]], cecr[['weeks']])
+            if(length(appearance) > 1 || !is.na(appearance)) {
+                appearances[[origin_as]] = appearance
+            }
+        }
+        appearances <<- appearances
     }
-
+    print("appearances done")
+    print("cgcr")
+    if(!exists('cgcr')) {
+        gcr <<- get.gcr(
+            names(appearances)[is.nottrue(as.integer(names(appearances)) > 0)])
+        cgcr <<- preprocess.gcr(gcr)
+    }
+    print("cgcr done")
+    # extract.data(appearances, cgcr)
 }
 
 
